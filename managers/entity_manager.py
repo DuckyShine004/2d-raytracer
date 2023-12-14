0748fe8fdbceb6d39b67ed9d0b0a3146b33ec31d
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-14 17:51:01 +1300

    bug: Fixed Incorrect SpeeRay Velocity

--- managers/entity_manager.py
+++ managers/entity_manager.py
@@ -6,7 +6,17 @@ import pygame
 from src.objects.ray import Ray
 from src.objects.point import Point
 
-from src.constants.constants import RAYS, RAY_X, RAY_Y, RADIUS, HALF_TAU
+from src.utilities.utility import Utility
+
+from src.constants.constants import (
+    FRAME_RATE,
+    SPEED,
+    RAYS,
+    RAY_X,
+    RAY_Y,
+    RADIUS,
+    HALF_TAU,
+)
 
 
 class EntityManager:
@@ -16,14 +26,13 @@ class EntityManager:
         self.rays = []
 
     def create_rays(self):
-        p1 = Point(RAY_X, RAY_Y)
-
         for ray_index in range(RAYS):
             theta = math.radians(HALF_TAU * (ray_index / RAYS))
 
             dx = RADIUS * math.cos(theta)
             dy = RADIUS * math.sin(theta)
 
+            p1 = Point(RAY_X, RAY_Y)
             p2 = Point(RAY_X + dx, RAY_Y + dy)
 
             self.rays.append(Ray(p1, p2))
@@ -43,7 +52,7 @@ class EntityManager:
         if keys[pygame.K_w]:
             dy = -1
 
-        if key[pygame.K_s]:
+        if keys[pygame.K_s]:
             dy = 1
 
         return Point(dx, dy)
@@ -52,7 +61,17 @@ class EntityManager:
         direction = self.get_direction()
 
         for ray in self.rays:
-            ...
+            normalization = Utility.get_normalization(direction.x, direction.y)
+
+            ax, ay = ray.p1.get_pos()
+            bx, by = ray.p2.get_pos()
+
+            vx = SPEED * direction.x * normalization
+            vy = SPEED * direction.y * normalization
+
+            print((ax, ay), (bx, by))
+            ray.set_pos(ray.p1, ax + vx, ay + vy)
+            ray.set_pos(ray.p2, bx + vx, by + vy)
 
     def render_rays(self):
         for ray in self.rays:
