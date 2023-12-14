4b66bead2ab78faa4bf1a6a71f8397d28f8e31aa
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-15 00:09:44 +1300

    refactor: Refactored Utility Class

--- src/utilities/utility.py
+++ src/utilities/utility.py
@@ -30,12 +30,7 @@ class Utility:
         return (a * d) - (b * c)
 
     @staticmethod
-    def get_intersection(p1, p2, p3, p4):
-        x1, y1 = p1.get_position()
-        x2, y2 = p2.get_position()
-        x3, y3 = p3.get_position()
-        x4, y4 = p4.get_position()
-
+    def get_determinants(x1, x2, x3, x4, y1, y2, y3, y4):
         a = Utility.get_determinant(x1, y1, x2, y2)
         b = Utility.get_determinant(x1, 1, x2, 1)
         c = Utility.get_determinant(x3, y3, x4, y4)
@@ -43,7 +38,24 @@ class Utility:
         e = Utility.get_determinant(y1, 1, y2, 1)
         f = Utility.get_determinant(y3, 1, y4, 1)
 
+        return [a, b, c, d, e, f]
+
+    @staticmethod
+    def get_intersection_point(determinants):
+        a, b, c, d, e, f = determinants
+
         px = Utility.get_determinant(a, b, c, d) / Utility.get_determinant(b, e, d, f)
         py = Utility.get_determinant(a, e, c, f) / Utility.get_determinant(b, e, d, f)
 
         return Point(px, py)
+
+    @staticmethod
+    def get_intersection(p1, p2, p3, p4):
+        x1, y1 = p1.get_position()
+        x2, y2 = p2.get_position()
+        x3, y3 = p3.get_position()
+        x4, y4 = p4.get_position()
+
+        determinants = Utility.get_determinants(x1, x2, x3, x4, y1, y2, y3, y4)
+
+        return Utility.get_intersection_point(determinants)
