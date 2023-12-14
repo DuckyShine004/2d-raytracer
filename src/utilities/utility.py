370b26e0a9a909b03c50d28ed89ee723350da0b2
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-15 00:01:07 +1300

    feat: Added Utility Intersection Function - Added get intersection method - Added get determinant method

--- src/utilities/utility.py
+++ src/utilities/utility.py
@@ -2,6 +2,8 @@ from __future__ import annotations
 
 import math
 
+from src.objects.point import Point
+
 
 class Utility:
     @staticmethod
@@ -22,3 +24,26 @@ class Utility:
         is_non_zero_component = Utility.check_non_zero_component(x, y)
 
         return 1 / magnitude if is_non_zero_component else 0
+
+    @staticmethod
+    def get_determinant(a, b, c, d):
+        return (a * d) - (b * c)
+
+    @staticmethod
+    def get_intersection(p1, p2, p3, p4):
+        x1, y1 = p1.get_position()
+        x2, y2 = p2.get_position()
+        x3, y3 = p3.get_position()
+        x4, y4 = p4.get_position()
+
+        a = Utility.get_determinant(x1, y1, x2, y2)
+        b = Utility.get_determinant(x1, 1, x2, 1)
+        c = Utility.get_determinant(x3, y3, x4, y4)
+        d = Utility.get_determinant(x3, 1, x4, 1)
+        e = Utility.get_determinant(y1, 1, y2, 1)
+        f = Utility.get_determinant(y3, 1, y4, 1)
+
+        px = Utility.get_determinant(a, b, c, d) / Utility.get_determinant(b, e, d, f)
+        py = Utility.get_determinant(a, e, c, f) / Utility.get_determinant(b, e, d, f)
+
+        return Point(px, py)
