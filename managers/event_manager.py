9f36bdddb3f9c03dc370e23038d801adf23d05a3
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-15 15:35:46 +1300

    docs: Added Docstrings To Event Manager

--- managers/event_manager.py
+++ managers/event_manager.py
@@ -1,24 +1,55 @@
+"""This module aims to handle all events that are related to peripherals.
+"""
+
 from __future__ import annotations
 
 import pygame
 
 
 class EventManager:
-    def __init__(self, app):
+
+    """The event manager will manage all events related to peripherals. For example, quitting an
+    application relies on detecting keyboard events.
+
+    Attributes:
+        app (App): The main application.
+    """
+
+    def __init__(self, app: App) -> None:
+        """The event manager constructor.
+
+        Args:
+            app (App): The main application.
+        """
+
         self.app = app
 
-    def handle_events(self):
+    def handle_events(self) -> None:
+        """Handle all events that are related to peripherals."""
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.app.is_running = False
 
             self.handle_keyboard_events(event)
 
-    def handle_keyboard_events(self, event):
+    def handle_keyboard_events(self, event: pygame.event.Event) -> None:
+        """Handle all keyboard events.
+
+        Args:
+            event (pygame.event.Event): The current event.
+        """
+
         if event.type == pygame.KEYDOWN:
             self.handle_keypress(event)
 
-    def handle_keypress(self, event):
+    def handle_keypress(self, event: pygame.event.Event) -> None:
+        """Handle key presses.
+
+        Args:
+            event (pygame.event.Event): The current event.
+        """
+
         match (event.key):
             case pygame.K_ESCAPE:
                 self.app.is_running = False
