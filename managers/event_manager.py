ca87c084c7a38ec831847311d1b18f931bc04afb
Gallon Zhou <gallonzhou04@gmail.com>
2023-12-15 15:20:49 +1300

    feat: Added Key Press To Generate Walls

--- managers/event_manager.py
+++ managers/event_manager.py
@@ -2,18 +2,25 @@ from __future__ import annotations
 
 import pygame
 
+
 class EventManager:
-	def __init__(self, app):
-		self.app = app
+    def __init__(self, app):
+        self.app = app
 
-	def handle_events(self):
-		for event in pygame.event.get():
-			self.handle_keyboard_events(event)
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.app.is_running = False
 
-	def handle_keyboard_events(self, event):
-		self.handle_quit_event(event)
+            self.handle_keyboard_events(event)
 
-	def handle_quit_event(self, event):
-		if event.type == pygame.QUIT:
-			self.app.is_running = False
+    def handle_keyboard_events(self, event):
+        if event.type == pygame.KEYDOWN:
+            self.handle_keypress(event)
 
+    def handle_keypress(self, event):
+        match (event.key):
+            case pygame.K_ESCAPE:
+                self.app.is_running = False
+            case pygame.K_r:
+                self.app.entity_manager.create_walls()
